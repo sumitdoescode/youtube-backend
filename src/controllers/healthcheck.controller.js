6f3b252b0747ac8f4c4215e6dfe078e8eca84eb8
Sumit Raj <sumit.does.code@gmail.com>
2025-05-26 03:18:40 +0530

    shit

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
+    console.log("health status is good");
     if (!req) {
         throw new ApiError(400, "health status is poor");
     }
