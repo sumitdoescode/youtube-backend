d005a477aef51d8269c25a9f73fbda68181c6ced
Sumit Raj <sumit.does.code@gmail.com>
2025-05-26 17:46:46 +0530

    fucking changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getCurrentUser, setUserAvatar, setUserCoverImage, getChannelDetails } from "../controllers/user.controller.js";
 import { requireAuth } from "@clerk/express";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 // prefix=  /api/v1/users
 router.get("/me", requireAuth(), getCurrentUser);
-router.patch("/me/avatar", requireAuth(), setUserAvatar);
-router.patch("/me/cover-image", requireAuth(), setUserCoverImage);
+router.patch("/me/avatar", requireAuth(), upload.single("avatar"), setUserAvatar);
+router.patch("/me/cover-image", requireAuth(), upload.single("coverImage"), setUserCoverImage);
 router.get("/channel/:username", requireAuth(), getChannelDetails);
 
 export default router;
