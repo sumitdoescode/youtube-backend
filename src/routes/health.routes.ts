6ff35774790e7c908585ed6f5a1457057b523102
sumitdoescode <sumit.does.code@gmail.com>
2026-04-22 16:48:40 +0530

    feat: add health check route to monitor server uptime and database connection status

--- src/routes/health.routes.ts
+++ src/routes/health.routes.ts
@@ -1,11 +1,11 @@
 import { Hono } from "hono";
-import mongoose from "mongoose";
+import { connection } from "mongoose";
 
 const router = new Hono();
 
 // GET => /api/health
 router.get("/", async (c) => {
-    return c.json({ success: true, message: "Server is running", uptime: process.uptime(), service: { db: mongoose.connection.readyState === 1 ? "connected" : "disconnected" } }, 200);
+    return c.json({ success: true, message: "Server is running", uptime: process.uptime(), service: { db: connection.readyState === 1 ? "connected" : "disconnected" } }, 200);
 });
 
 export default router;
