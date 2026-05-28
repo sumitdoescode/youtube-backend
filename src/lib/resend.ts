63430cc3ff19b6ebce7d87eb82d1e72f6b234113
sumitdoescode <sumit.does.code@gmail.com>
2026-05-28 23:32:04 +0530

    feat: initialize Resend client with environment configuration

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
 
 if (!RESEND_API_KEY) {
-    throw new Error("Please provide RESEND_API_KEY in the environment variables");
+  throw new Error("Please provide RESEND_API_KEY in the environment variables");
 }
 
 const resend = new Resend(RESEND_API_KEY);
